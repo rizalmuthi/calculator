1796587407049308ce31b7a3adf05f4a879f32df
Rizal Muthi <rizalmuthi@gmail.com>
2024-02-15 12:20:40 +1100

    Implement the logic calculation

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -49,5 +49,35 @@ class Calculator
   end
 
   def calculate
+    final_balance = start_deposit
+    interest_rate_decimal = interest_rate / 100.0
+
+    case interest_paid_type
+    when :monthly
+      periods = investment_term * 12
+      monthly_interest_rate = interest_rate_decimal / 12
+      periods.times do
+        final_balance += final_balance * monthly_interest_rate
+      end
+    when :quarterly
+      periods = investment_term * 4
+      quarterly_interest_rate = interest_rate_decimal / 4
+      periods.times do
+        final_balance += final_balance * quarterly_interest_rate
+      end
+    when :annually
+      periods = investment_term
+      periods.times do
+        final_balance += final_balance * interest_rate_decimal
+      end
+    when :at_maturity
+      final_balance += final_balance * (interest_rate_decimal * investment_term)
+    else
+      raise InvalidInterestPaidTypeError.new(
+        "Invalid Interest Paid Type. Must be one of: #{INTEREST_PAID_TYPE.join(" ")}"
+      )
+    end
+
+    final_balance.round(2)
   end
 end
