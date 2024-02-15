bb72ee7a8f0e18fc22f0d4df570661db9ccb62ea
Rizal Muthi <rizalmuthi@gmail.com>
2024-02-15 12:09:00 +1100

    Add validation tests

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -24,27 +24,42 @@ describe Calculator do
 
     context "when the input is invalid" do
       context "and start deposit is not positive number" do
+        let(:start_deposit) { -100 }
+
         it "raises InvalidDepositError" do
+          expect { calculator.run }.to raise_error InvalidDepositError
         end
       end
 
       context "and interest rate is not positive number" do
+        let(:interest_rate) { -1.5 }
+
         it "raises InvalidInterestRateError" do
+          expect { calculator.run }.to raise_error InvalidInterestRateError
         end
       end
 
       context "and investment term is not positive number" do
+        let(:investment_term) { -10 }
+
         it "raises InvalidInvestmentTermRateError" do
+          expect { calculator.run }.to raise_error InvalidInvestmentTermRateError
         end
       end
 
       context "and investment term is not whole number" do
+        let(:investment_term) { 12.5 }
+
         it "raises InvalidInvestmentTermRateError" do
+          expect { calculator.run }.to raise_error InvalidInvestmentTermRateError
         end
       end
 
       context "and interest paid type is not valid" do
+        let(:interest_paid_type) { :something_else }
+
         it "raises InvalidInterestPaidTypeError" do
+          expect { calculator.run }.to raise_error InvalidInterestPaidTypeError
         end
       end
     end
