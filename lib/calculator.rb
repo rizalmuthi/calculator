0e27cbe54a2b3868f3820082b8946e46a15a5f16
Rizal Muthi <rizalmuthi@gmail.com>
2024-02-15 13:40:18 +1100

    Refactor, break into smaller methods

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -32,32 +32,36 @@ class Calculator
     final_balance = start_deposit
     interest_rate_decimal = interest_rate / 100.0
 
+    periods.times do
+      final_balance += final_balance * monthly_interest_rate(interest_rate_decimal)
+    end
+
+    final_balance.round(0)
+  end
+
+  def periods
     case interest_paid_type
-    when :monthly
-      periods = investment_term * 12
-      monthly_interest_rate = interest_rate_decimal / 12
-      periods.times do
-        final_balance += final_balance * monthly_interest_rate
-      end
-    when :quarterly
-      periods = investment_term * 4
-      quarterly_interest_rate = interest_rate_decimal / 4
-      periods.times do
-        final_balance += final_balance * quarterly_interest_rate
-      end
-    when :annually
-      periods = investment_term
-      periods.times do
-        final_balance += final_balance * interest_rate_decimal
-      end
-    when :at_maturity
-      final_balance += final_balance * (interest_rate_decimal * investment_term)
+    when :monthly then investment_term * 12
+    when :quarterly then investment_term * 4
+    when :annually then investment_term
+    when :at_maturity then 1
     else
       raise InvalidInterestPaidTypeError.new(
         "Invalid Interest Paid Type. Must be one of: #{Validator::INTEREST_PAID_TYPE.join(" ")}"
       )
     end
+  end
 
-    final_balance.round(0)
+  def monthly_interest_rate(interest_rate_decimal)
+    case interest_paid_type
+    when :monthly then interest_rate_decimal / 12
+    when :quarterly then interest_rate_decimal / 4
+    when :annually then interest_rate_decimal
+    when :at_maturity then interest_rate_decimal * investment_term
+    else
+      raise InvalidInterestPaidTypeError.new(
+        "Invalid Interest Paid Type. Must be one of: #{Validator::INTEREST_PAID_TYPE.join(" ")}"
+      )
+    end
   end
 end
